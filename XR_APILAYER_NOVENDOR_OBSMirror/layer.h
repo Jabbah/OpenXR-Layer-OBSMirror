30fa9ed77b7e0e98b06b8b2d6d53dc4ce17a2c85
Jabbah <jim@jameslacey.co.uk>
2022-12-20 19:42:40 +0000

    Update version

--- XR_APILAYER_NOVENDOR_OBSMirror/layer.h
+++ XR_APILAYER_NOVENDOR_OBSMirror/layer.h
@@ -27,7 +27,7 @@
 namespace layer_OBSMirror {
 
     const std::string LayerName = "XR_APILAYER_NOVENDOR_OBSMirror";
-    const std::string VersionString = "Beta (0.1.0)";
+    const std::string VersionString = "(v0.2.0-beta)";
 
     // Singleton accessor.
     OpenXrApi* GetInstance();
