f98a726f3c59c87c251ccc625300c214a681ac90
Antti Keränen <detegr@gmail.com>
2025-02-11 12:46:53 +0200

    Add support for 32-bit builds

--- XR_APILAYER_NOVENDOR_OBSMirror/framework/dispatch.h
+++ XR_APILAYER_NOVENDOR_OBSMirror/framework/dispatch.h
@@ -28,10 +28,10 @@
 
 namespace LAYER_NAMESPACE {
 
-    XrResult xrGetInstanceProcAddr(XrInstance instance,
+    XrResult XRAPI_CALL xrGetInstanceProcAddr(XrInstance instance,
                                    const char* name,
                                    PFN_xrVoidFunction* function);
-    XrResult xrCreateApiLayerInstance(const XrInstanceCreateInfo* instanceCreateInfo,
+    XrResult XRAPI_CALL xrCreateApiLayerInstance(const XrInstanceCreateInfo* instanceCreateInfo,
                                       const struct XrApiLayerCreateInfo* apiLayerInfo,
                                       XrInstance* instance);
 
